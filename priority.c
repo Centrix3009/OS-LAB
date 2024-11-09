7b02da62415282222351512e0f5ed02590f9c8ca
Devanshu Pareek <devanshupareek3009@gmail.com>
2024-11-09 14:23:26 +0530

    Describe the changes you made

--- priority.c
+++ priority.c
@@ -1,107 +1,102 @@
 #include <stdio.h>
 
-//Function to swap two variables
+// Function to swap two variables
 
-void swap(int *a, int *b)
+void swap(int* a, int* b)
 
 {
 
-int temp=*a;
+    int temp = *a;
 
-*a=*b;
+    *a = *b;
 
-*b=temp;
-
+    *b = temp;
 }
 
 int main()
 
 {
 
-int n;
-printf("Enter Number of Processes: ");
+    int n;
+    printf("Enter Number of Processes: ");
 
-scanf("%d",&n);
+    scanf("%d", &n);
 
-// b is array for burst time, p for priority and index for process id
+    // b is array for burst time, p for priority and index for process id
 
-int b[n],p[n], index[n];
+    int b[n], p[n], index[n];
 
-for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
 
-{
+    {
 
-printf("Enter Burst Time and Priority Value for Process %d: ",i+1);
+        printf("Enter Burst Time and Priority Value for Process %d: ", i + 1);
 
-scanf("%d %d",&b[i],&p[i]);
+        scanf("%d %d", &b[i], &p[i]);
 
-index[i]=i+1;
+        index[i] = i + 1;
+    }
 
-}
+    for (int i = 0; i < n; i++)
 
-for(int i=0;i<n;i++)
+    {
 
-{
+        int a = p[i], m = i;
 
-int a=p[i],m=i;
+        // Finding out highest priority element and placing it at its desired
+        // position
 
-//Finding out highest priority element and placing it at its desired position
+        for (int j = i; j < n; j++)
 
-for(int j=i;j<n;j++)
+        {
 
-{
+            if (p[j] > a)
 
-if(p[j] > a)
+            {
 
-{
+                a = p[j];
+                m = j;
+            }
+        }
 
-a=p[j];
-m=j;
+        // Swapping processes
 
-}
+        swap(&p[i], &p[m]);
 
-}
+        swap(&b[i], &b[m]);
 
-//Swapping processes
+        swap(&index[i], &index[m]);
+    }
 
-swap(&p[i], &p[m]);
+    // T stores the starting time of process
 
-swap(&b[i], &b[m]);
+    int t = 0;
+    // Printing scheduled process
 
-swap(&index[i],&index[m]);
+    printf("Order of process Execution is\n");
 
-}
+    for (int i = 0; i < n; i++)
 
-// T stores the starting time of process
+    {
 
-int t=0;
-//Printing scheduled process
+        printf("P%d is executed from %d to %d\n", index[i], t, t + b[i]);
+        t += b[i];
+    }
 
-printf("Order of process Execution is\n");
+    printf("\n");
 
-for(int i=0;i<n;i++)
+    printf("Process Id Burst Time Wait Time TurnAround Time\n");
 
-{
+    int wait_time = 0;
 
-printf("P%d is executed from %d to %d\n", index[i],t,t+b[i]); t+=b[i];
-}
+    for (int i = 0; i < n; i++)
 
-printf("\n");
+    {
 
-printf("Process Id Burst Time Wait Time TurnAround Time\n");
+        printf("P%d %d %d %d\n", index[i], b[i], wait_time, wait_time + b[i]);
 
-int wait_time=0;
+        wait_time += b[i];
+    }
 
-for(int i=0;i<n;i++)
-
-{
-
-printf("P%d %d %d %d\n",index[i],b[i],wait_time,wait_time + b[i]);
-
-wait_time += b[i];
-
-}
-
-return 0;
-
+    return 0;
 }
